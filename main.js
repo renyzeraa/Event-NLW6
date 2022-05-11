7ec62811fb423a1a8416c925b89240cb1858a42e
Renan Silva <renansilvaytB@gmail.com>
2022-05-10 22:12:23 -0300

    alterações layout e funcionalidades js

---
+++ main.js
@@ -0,0 +1,42 @@
+// char (caracteres) na programção é chamada de string
+// 'um texto aqui';
+
+// number (numero)
+// 12345;
+
+// boolean (dado logico)
+// true or false;
+
+//extruturar dados
+
+//ABRE E FECHA O MENU
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//mudar o header da pagina quando der scroll
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
