3bbc1b86bf718eebd060546f166438da47956a4d
Renan Silva <renansilvaytB@gmail.com>
2022-05-11 16:38:39 -0300

    atualizações css e funções

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ for (const element of toggle) {
   })
 }
 
-//
+//quando clicar em um item do menu, esconder o menu
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -40,3 +40,31 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
