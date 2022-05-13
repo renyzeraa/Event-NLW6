b530455834d65312d03593ea154781b0cb75f65d
Renan Silva <renansilvaytB@gmail.com>
2022-05-13 16:35:48 -0300

    Finalização do projeto

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 
 //extruturar dados
 
-//ABRE E FECHA O MENU
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -19,7 +19,7 @@ for (const element of toggle) {
   })
 }
 
-//quando clicar em um item do menu, esconder o menu
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -28,18 +28,19 @@ for (const link of links) {
   })
 }
 
-//mudar o header da pagina quando der scroll
-
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (this.window.scrollY >= navHeight) {
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -48,7 +49,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -64,18 +71,51 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links, footer .brand, footer .social
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
 
-/* button back to top */
-
+/* Botão voltar para o topo */
 const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
+
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
---
+++ readme.md
@@ -0,0 +1,7 @@
+Ola, seja bem vindo a mais um projeto.
+
+Foi executado no evento NLW 6 edição da RocketSeat de 2021.
+
+Intuito era criar uma page, com varias funções de JavaScript, com uma modelagem css simples, mas completa.
+
+Segue o link do site no ar:
